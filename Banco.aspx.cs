ebefa759baba692c8cbd944e227c3ecbe2686305
Caio Cesar <caio.sfg@gmail.com>
2018-12-04 04:28:32 -0200

    Criada conexão ao banco

---
+++ Banco.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace Introducao
+{
+    public partial class Banco : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CheckMysqlConnection();
+            }
+        }
+        public void CheckMysqlConnection()
+        {
+            string connectionString = @"Data Source=localhost; Database=cadastro; User ID=root; Password='root'";
+            using (MySqlConnection cn = new MySqlConnection(connectionString))
+            {
+                cn.Open();
+                Response.Write("Conexão com sucesso ao Banco de Dados");
+            }
+        }
+    }
+}
--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace Introducao.Controllers
 {
     public class UsuarioController : Controller
